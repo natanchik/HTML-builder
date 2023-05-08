b210a879634380a99d78f1704816a1d12f696fa6
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-05-08 09:45:47 +0600

    task 4: add remove folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,34 +2,33 @@ const path = require('path');
 const fsPromises = require('fs/promises');
 const fs = require('fs');
 
-let pathFrom = path.join(__dirname, 'files');
-let pathTo = path.join(__dirname, 'files-copy');
+const pathFrom = path.join(__dirname, 'files');
+const pathTo = path.join(__dirname, 'files-copy');
 
-//let rmDir = fsPromises.rm(pathTo, { force: true, recursive: true }, (err) => { if (err) throw err });              
-
-function addFiles(pathFrom, pathTo) { // Take the list of files
-  fsPromises.mkdir(pathTo, { recursive: true }).then(() => {
-    fs.readdir(pathFrom, (err, files) => {          // Get the list of files and folders
-      if (err) throw err;           
-      for (let file of files) {                               // Go by the list of files
-        fsPromises.stat(path.join(pathFrom, file))      // Get info about file or folder
-          .then((stats) => {
-            if (stats.isFile()) {
-              fs.copyFile(path.join(pathFrom, file), path.join(pathTo, file), (err) => {
-                if (err) throw err;
-              });
-            } if (stats.isDirectory()) {    
-              let newPathTo = path.join(pathTo, file); 
-              let newPathFrom = path.join(pathFrom, file); 
-              addFiles(newPathFrom, newPathTo)                         // start new loop
-            }
-          });
-      }
-    })
-  })  
+function addFiles(pathFrom, pathTo) {                           
+  fsPromises.rm(pathTo, { force: true, recursive: true }).then(() => {    // remove folder
+    fsPromises.mkdir(pathTo, { recursive: true }).then(() => {           // add new folder
+      fs.readdir(pathFrom, (err, files) => {          // Get the list of files and folders
+        if (err) throw err;           
+        for (const file of files) {                               // Go by the list of files
+          fsPromises.stat(path.join(pathFrom, file))      // Get info about file or folder
+            .then((stats) => {
+              if (stats.isFile()) {
+                fs.copyFile(path.join(pathFrom, file), path.join(pathTo, file), (err) => {
+                  if (err) throw err;
+                });
+              } if (stats.isDirectory()) {    
+                const newPathTo = path.join(pathTo, file); 
+                const newPathFrom = path.join(pathFrom, file); 
+                addFiles(newPathFrom, newPathTo);                        // start new loop
+              }
+            });
+        }
+      });
+    });
+  });
 }
 
-//rmDir.then(() => 
 addFiles(pathFrom, pathTo);
 
 // dir -> list of elemetns -> loop with create elements -> if dir
